4bd34dd38fde22256e30692f50e30e23a8742deb
Patel Krushankant <patelkk1618@gmail.com>
2020-07-09 16:11:57 -0500

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {PromiseComponent} from "./promise/promise.component";
+
+
+const routes: Routes = [
+  {path: 'promise', component: PromiseComponent},
+  {path: '**', redirectTo:'promise'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
