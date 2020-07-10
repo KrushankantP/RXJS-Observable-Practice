2b12a7c9216af4f837d9fea5679a943d3ce6805b
Patel Krushankant <patelkk1618@gmail.com>
2020-07-09 20:41:02 -0500

    JavaScript Promise

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -6,10 +6,65 @@
   styleUrls: ['./promise.component.scss']
 })
 export class PromiseComponent implements OnInit {
+  promiseVAl:string
+  dell={
+    brand: 'Dell',
+    hardDisk: '2 TB',
+    color: 'black'
+  }
+  hp={
+    brand:'Hp',
+    hardDisk: '1 TB',
+    color: 'silver'
+  }
+  notAvil={
+    brand:'Not Available',
+    status:'Failed'
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  DellAvailable(){
+    return false
   }
+  HpAvailable(){
+    return true
+  }
+
+
+  ngOnInit(): void {
+    //let buyLaptop = new Promise(function (resolve, reject){
+    //resolve('Promise is resolved')
+    //});
+
+    let buyLaptop = new Promise((resolve, reject)=>{
+      //resolve('Promise is resolved');
+      //reject('promise is reject');
+      if(this.DellAvailable()){
+        return setTimeout(()=>{
+          //resolve('Dell is Purchased')
+          resolve(this.dell)
+        }, 3000)
+      }else if(this.HpAvailable()){
+        return setTimeout(()=>{
+          //resolve('Hp is purchased')
+          resolve(this.hp)
+        }, 3000)
+      }else
+        {
+        return setTimeout(()=>{
+         // reject('Laptop is not available on Store')
+          reject(this.notAvil)
+        }, 3000)
+      }
+    })
+
+    buyLaptop.then(res=>{
+      console.log('then code =>', res )
+    }).catch(res=>{
+      console.log('catch code=>', res)
+    })
+  }
+
 
 }
