edd02974edf2f5b6141c660f4b23c562b792bb03
Patel Krushankant <patelkk1618@gmail.com>
2020-08-08 12:13:14 -0500

    Interval Operator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {PromiseComponent} from "./promise/promise.component";
 import {ObservableComponent} from "./observable/observable.component";
 import {ListComponent} from './observable/list/list.component';
 import {FromEventComponent} from './observable/from-event/from-event.component';
+import {IntervalComponent} from './observable/interval/interval.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,8 @@ const routes: Routes = [
   {path: 'observable', component: ObservableComponent, children:[
       {path: '', component:ListComponent},
       {path: 'fromEvent', component:FromEventComponent},
+      {path: 'interval', component:IntervalComponent},
+
     ]},
   {path: '**', redirectTo:'promise'}
 ];
