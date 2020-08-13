ed38efceef40e0760e8d42d3f530b3865bd47ed5
Patel Krushankant <patelkk1618@gmail.com>
2020-08-12 22:54:01 -0500

    Map Operator Ex-2 and Ex-3

--- src/app/observable/map/map.component.ts
+++ src/app/observable/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {interval, Subscription} from 'rxjs';
+import {from, interval, Subscription} from 'rxjs';
 import {map} from 'rxjs/operators';
+import {DesignUtilityService} from '../../appServices/design-utility.service';
 
 @Component({
   selector: 'app-map',
@@ -11,9 +12,11 @@ export class MapComponent implements OnInit {
 
   //subscription
   sub1: Subscription;
+  sub2: Subscription;
   //Messages
   msg1;
-  constructor() { }
+  msg2;
+  constructor(private _designUtility : DesignUtilityService) { }
 
   ngOnInit(): void {
 
@@ -32,6 +35,40 @@ export class MapComponent implements OnInit {
         this.sub1.unsubscribe();
       },10000);
 
+
+    // Ex - 02
+
+    this.sub2 = broadCastVideos.pipe(
+      map(data => data * 3)
+    )
+      .subscribe(res =>{
+        //console.log(res);
+        this.msg2 = res;
+      })
+
+    setTimeout(()=>{
+      this.sub2.unsubscribe();
+    },10000);
+
+
+    // Ex - 03
+
+    const members = from([
+      {id:1, name:'Krishna'},
+      {id:2, name:'Patel'},
+      {id:3, name:'Atmiyata'},
+      {id:4, name:'Nixal'},
+      {id:5, name:'Niramay'},
+      {id:6, name:'Gunatit'},
+      {id:7, name:'Akhshar'},
+      {id:8, name:'Yogi'},
+    ])
+
+    members.pipe(map(data=> data.name))
+    .subscribe(res=>{
+      console.log(res);
+      this._designUtility.print(res, 'elContainer')
+    })
   }
 
 }
