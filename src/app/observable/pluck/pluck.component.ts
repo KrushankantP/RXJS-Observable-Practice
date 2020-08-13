f59ce6e5117270b7dad84199dc3aacdb1bdb1599
Patel Krushankant <patelkk1618@gmail.com>
2020-08-12 23:46:14 -0500

    Pluck Operator Ex-2 By nested Property

--- src/app/observable/pluck/pluck.component.ts
+++ src/app/observable/pluck/pluck.component.ts
@@ -8,18 +8,53 @@ import {pluck, toArray} from 'rxjs/operators';
   styleUrls: ['./pluck.component.scss']
 })
 export class PluckComponent implements OnInit {
-  users=[
-    {name:'Krishna', skills: 'Angular'},
-    {name:'Patel', skills: 'HTML, CSS'},
-    {name:'Nixal', skills: 'TypeScript'},
-    {name:'Niramay', skills: 'JavaScript'}
-  ]
   data;
+  data2;
+  users=[
+    {
+      name:'Krishna',
+      skills: 'Angular',
+      job:{
+        title:'frontend Developer',
+        exp: '10 Years'
+      }
+    },
+    {
+      name:'Patel',
+      skills: 'HTML, CSS',
+      job:{
+        title:'HTML CSS Developer',
+        exp: '11 Years'
+      }
+    },
+
+    {
+      name:'Nixal',
+      skills: 'TypeScript',
+      job:{
+        title:'Typescript Developer',
+        exp: '10 Years'
+      }
+    },
+
+    { name:'Niramay',
+      skills: 'JavaScript',
+      job:{
+        title:'JavaScript Developer',
+        exp: '12 Years'
+      }
+    }
+  ]
+
+
 
   constructor() { }
 
 
   ngOnInit(): void {
+
+    //Ex - 01 Pluck by Property
+
     from(this.users).pipe(
       //map(data=> data.name), OR
       pluck('name'),
@@ -30,6 +65,16 @@ export class PluckComponent implements OnInit {
         this.data = res;
       })
 
+
+    //Ex - 02 Pluck by nested Property
+    from(this.users).pipe(
+      pluck('job', 'title'),
+      toArray()
+    )
+      .subscribe(res=>{
+        console.log(res);
+        this.data2 = res;
+      })
   }
 
 }
